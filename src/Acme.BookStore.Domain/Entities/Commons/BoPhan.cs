ab3e6f0aafb35b7cbfe03b589b453626b5f57d4a
juicyDD <ongnguyenuyennhi@gmail.com>
2021-11-22 21:57:20 +0700

    first commit

---
+++ src/Acme.BookStore.Domain/Entities/Commons/BoPhan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Entities.Commons
+{
+    public class BoPhan : FullAuditedAggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+        public string GhiChu { get; set; }
+        public List<NhanVien> NhanViens { get; set; }
+    }
+}
